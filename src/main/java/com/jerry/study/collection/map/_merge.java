ae696801bc86b226f022eae2f9e6e6b9afa07a9f
jerry <jerry.xiaojunhe@gmail.com>
2019-09-29 11:30:34 +0800

    lambda

---
+++ src/main/java/com/jerry/study/collection/map/_merge.java
@@ -0,0 +1,22 @@
+package com.jerry.study.collection.map;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @auther Jerry
+ */
+public class _merge {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> words = Arrays.asList("aa", "bb", "cc", "aa", "cc");
+        words.forEach(word -> {
+                map.merge(word, 1, (previousValue, newValue) -> previousValue + newValue);
+            }
+        );
+
+        System.out.println(map);
+    }
+}
